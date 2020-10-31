528e5945fedc233dd9c66dac86e4a28dd194097e
tom_pro <goodMorning_pro@atguigu.com>
2020-10-31 23:42:06 +0800

    version1

---
+++ book/src/com/hit/service/impl/UserServiceImpl.java
@@ -0,0 +1,33 @@
+package com.hit.service.impl;
+
+import com.hit.dao.UserDao;
+import com.hit.dao.impl.UserDaoImpl;
+import com.hit.pojo.User;
+import com.hit.service.UserService;
+
+public class UserServiceImpl implements UserService {
+
+    private UserDao userDao = new UserDaoImpl();
+
+    @Override
+    public void registUser(User user) {
+        userDao.saveUser(user);
+    }
+
+    @Override
+    public User login(User user) {
+        return userDao.queryUserByUsernameAndPassword(user.getUsername(), user.getPassword());
+    }
+
+    @Override
+    public boolean existsUsername(String username) {
+
+        if (userDao.queryUserByUsername(username) == null) {
+           // 等于null,说明没查到，没查到表示可用
+           return false;
+        }
+
+        return true;
+
+    }
+}
