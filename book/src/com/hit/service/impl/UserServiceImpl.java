82c622c5ee8dd69580b09c17884c1e2685e8c75c
tom_pro <goodMorning_pro@atguigu.com>
2020-11-01 09:52:10 +0800

    version

--- book/src/com/hit/service/impl/UserServiceImpl.java
+++ book/src/com/hit/service/impl/UserServiceImpl.java
@@ -1,9 +1,6 @@
 package com.hit.service.impl;
 
-import com.hit.dao.UserDao;
-import com.hit.dao.impl.UserDaoImpl;
-import com.hit.pojo.User;
-import com.hit.service.UserService;
+
 
 public class UserServiceImpl {
 
